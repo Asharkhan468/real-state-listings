3988d882887828fb0fdead5b9d1e5f68b3ae2253
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-11-15 18:09:56 +0500

    SOME CODE ADD

--- proxy.ts
+++ proxy.ts
@@ -6,7 +6,7 @@ export function proxy(request: NextRequest) {
 
   if (protectedRoutes.some(route => request.nextUrl.pathname.startsWith(route))) {
     if (!token) {
-      return NextResponse.redirect(new URL('/login', request.url));
+      return NextResponse.redirect(new URL('/admin', request.url));
     }
     
   }
