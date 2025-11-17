a04079c5b17ba69b5d676a35cfe16273a9143aba
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-11-17 18:59:06 +0500

    MIDDLEWARE ADD

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+
+  const protectedRoutes = ["/addProperty", "/adminProperty"];
+
+  const currentPath = req.nextUrl.pathname;
+
+  // Check if route is protected
+  if (protectedRoutes.some((route) => currentPath.startsWith(route))) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/admin", req.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/addProperty/:path*", "/adminProperty/:path*"],
+};
