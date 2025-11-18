d23daf8228cff8ff227ef1cc8d31d3f43528bb6c
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-11-18 15:45:14 +0500

    LAYOUT FILE ADD

--- app/addProperty/layout.tsx
+++ app/addProperty/layout.tsx
@@ -2,13 +2,11 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedLayout({ children }: any) {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();   
   const token = cookieStore.get("token")?.value;
 
-  console.log("token ==>" , token);
-
   if (!token) {
-    redirect("/admin");
+    redirect("/admin");                     
   }
 
   return <>{children}</>;
